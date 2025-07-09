2e50a306f59ab3c8aed16b114300907ab01a3678
LL <nishuixunzhou@qq.com>
2025-07-09 09:22:34 +0800

    注册路由

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,29 @@
 package routes
+
+import (
+	"GoBlog/utils"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+//go语言对于函数是否私有，通过函数名首字母大小写来判断
+//函数名首字母大写，则该函数是公共函数
+//函数名首字母小写，则该函数是私有函数
+
+func InitRouter() {
+	//设置gin的运行模式
+	gin.SetMode(utils.AppMode)
+	//创建路由
+	r := gin.Default()
+
+	router := r.Group("/api/v1")
+	{
+		router.GET("hello", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"msg": "ok",
+			})
+		})
+	}
+
+	r.Run(utils.HttpPort)
+}
